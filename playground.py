8ec513d67bd738ed2750cbf18c58fcb29f81fb88
mattng23 <mattng39@gmail.com>
2026-08-26 15:37:05 -0500

    Save progress

--- playground.py
+++ playground.py
@@ -33,3 +33,79 @@ shots[[
     "shot.statsbomb_xg",
     "shot.outcome.name"
 ]]
+
+#Extract columns we want
+
+cols = [
+    "index",
+    "possession",
+    "possession_team.name",
+    "team.name",
+    "minute",
+    "second",
+    "player.name",
+    "type.name",
+    "x",
+    "y",
+    "shot.statsbomb_xg",
+    "shot.outcome.name"
+]
+
+df[cols].head(80)
+
+# Take out usesless values
+
+df = df[
+    ~df["type.name"].isin([
+        "Half Start",
+        "Starting XI",
+        "Half End",
+        "Substitution",
+        "Tactical Shift"
+    ])
+].copy()
+
+# Only want actions done by the team in possession for now
+
+df = df[
+    df["team.name"] == df["possession_team.name"]
+].copy()
+
+# Half Start and Starting XI counted as possession 1 (which we have now taken out). We want to make the first true possession = 1 
+
+df["possession"] = df["possession"] - 1
+
+# Create location and info for where current event ends (i.e where the next event starts)
+
+df["next_x"] = (
+    df.groupby("possession")["x"].shift(-1)
+)
+
+df["next_y"] = (
+    df.groupby("possession")["y"].shift(-1)
+)
+
+df["next_type"] = (
+    df.groupby("possession")["type.name"].shift(-1)
+)
+
+df["next_player"] = (
+    df.groupby("possession")["player.name"].shift(-1)
+)
+
+# Incorporate new columns
+
+sequence_cols = [
+    "possession",
+    "possession_event_num",
+    "player.name",
+    "type.name",
+    "x",
+    "y",
+    "next_player",
+    "next_type",
+    "next_x",
+    "next_y"
+]
+
+df[df["possession"] == 1][sequence_cols]
